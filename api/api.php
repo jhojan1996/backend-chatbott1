1598a45a5711ebaf62cd1d0025a5f876d4e8624a
Innovati1\Jhojan Garcia <jhojan@consultoriagp.co>
2017-09-01 13:49:24 -0500

    api upload file

--- api/api.php
+++ api/api.php
@@ -3,17 +3,24 @@
 $tmp_name = $_FILES["data"]["tmp_name"];
 $upload_name = $_FILES["data"]["name"];
 $type = $_FILES["data"]["type"];
-$filename = "test3.wav";
+$filename = date("Y-m-d h:i:s").".wav";
 $saved = 0;
 
 $saved = move_uploaded_file($tmp_name, $filename) ? 1 : 0;
 
-$response = array(
-	"status"=>"200",
-	"msj"=>"Success"
-);
+if($saved == 1){
+	$response = array(
+		"status"=>"200",
+		"msj"=>"Success",
+		"url"=>"https://backend-chatbott1.scm.azurewebsites.net/api/".$filename
+	);
+}else{
+	$response = array(
+		"status"=>"500",
+		"msj"=>"Error guardando el archivo",
+	);
+}
 
-var_dump($_FILES);
 
 echo json_encode($response);
 
