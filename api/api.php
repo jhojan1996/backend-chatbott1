19d0054527c4e8622d58dcf520ee72696ba2dc58
Innovati1\Jhojan Garcia <jhojan@consultoriagp.co>
2017-09-01 12:22:17 -0500

    api upload file

--- api/api.php
+++ api/api.php
@@ -1,3 +1,18 @@
 <?php
-	echo "Hola mundo";
+
+$tmp_name = $_FILES["blob"]["tmp_name"];
+$upload_name = $_FILES["blob"]["name"];
+$type = $_FILES["blob"]["type"];
+$filename = "test3.wav";
+$saved = 0;
+
+$saved = move_uploaded_file($tmp_name, $filename) ? 1 : 0;
+
+$response = array(
+	"status"=>"200",
+	"msj"=>"Success"
+);
+
+echo json_encode($response);
+
 ?>
